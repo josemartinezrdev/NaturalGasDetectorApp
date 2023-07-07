b86836e8a927cf167137d1763cd509ede48dd9e1
José Martínez <josemartinezrdev@gmail.com>
2023-07-06 20:05:17 -0500

    NGD-657 BT-Connection-Completed

--- app/src/main/java/com/example/naturalgasdetector/ConexionActivity.kt
+++ app/src/main/java/com/example/naturalgasdetector/ConexionActivity.kt
@@ -1,11 +1,161 @@
 package com.example.naturalgasdetector
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import android.Manifest
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.os.Message
+import androidx.core.app.ActivityCompat
+import java.io.IOException
+import java.util.UUID
 
 class ConexionActivity : AppCompatActivity() {
+
+    val REQUEST_ENABLE_BT = 1
+
+    lateinit var mBtAdapter: BluetoothAdapter
+    var mAddressDevices: ArrayAdapter<String>? = null
+    var mNameDevices: ArrayAdapter<String>? = null
+
+    companion object {
+        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        private var m_bluetoothSocket: BluetoothSocket? = null
+
+        var m_isConnected: Boolean = false
+        lateinit var m_address: String
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conexion)
+
+        mAddressDevices = ArrayAdapter(this, android.R.layout.simple_list_item_1)
+        mNameDevices = ArrayAdapter(this, android.R.layout.simple_list_item_1)
+
+
+        val btnActBT = findViewById<Button>(R.id.btnActBT)
+        val btnDesBT = findViewById<Button>(R.id.btnDesBT)
+        val btnDispBT = findViewById<Button>(R.id.btnDispBT)
+        val listDispBT = findViewById<Spinner>(R.id.listDispBT)
+        val btnDispConnect = findViewById<Button>(R.id.btnDispConnect)
+        val campSSID = findViewById<EditText>(R.id.campSSID)
+        val campPASS = findViewById<EditText>(R.id.campPASS)
+        val btnSendDW = findViewById<Button>(R.id.btnSendDW)
+
+        val someActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == REQUEST_ENABLE_BT) {
+                Log.i("MainActivity", "ACTIVIDAD REGISTRADA")
+            }
+        }
+
+        mBtAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+
+        //Checar si esta encendido o apagado
+        if (mBtAdapter == null) {
+            Toast.makeText(
+                this,
+                "Bluetooth no está disponible en este dipositivo",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            Toast.makeText(this, "Bluetooth está disponible en este dispositivo", Toast.LENGTH_LONG)
+                .show()
+        }
+
+        btnActBT.setOnClickListener {
+            if (mBtAdapter.isEnabled) {
+                //Si ya está activado
+                Toast.makeText(this, "Bluetooth ya se encuentra activado", Toast.LENGTH_LONG).show()
+            } else {
+                //Encender Bluetooth
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    Log.i("MainActivity", "ActivityCompat#requestPermissions")
+                }
+                someActivityResultLauncher.launch(enableBtIntent)
+            }
+        }
+
+        btnDesBT.setOnClickListener {
+            if (!mBtAdapter.isEnabled) {
+                //Si ya está desactivado
+                Toast.makeText(this, "Bluetooth ya se encuentra desactivado", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                //Encender Bluetooth
+                mBtAdapter.disable()
+                Toast.makeText(this, "Se ha desactivado el bluetooth", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        btnDispBT.setOnClickListener {
+            if (mBtAdapter.isEnabled) {
+
+                val pairedDevices: Set<BluetoothDevice>? = mBtAdapter?.bondedDevices
+                mAddressDevices!!.clear()
+                mNameDevices!!.clear()
+
+                pairedDevices?.forEach { device ->
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                    mAddressDevices!!.add(deviceHardwareAddress)
+                    //........... EN ESTE PUNTO GUARDO LOS NOMBRE A MOSTRARSE EN EL COMBO BOX
+                    mNameDevices!!.add(deviceName)
+                }
+
+                //ACTUALIZO LOS DISPOSITIVOS
+                listDispBT.setAdapter(mNameDevices)
+            } else {
+                val noDevices = "Ningun dispositivo pudo ser emparejado"
+                mAddressDevices!!.add(noDevices)
+                mNameDevices!!.add(noDevices)
+                Toast.makeText(this, "Primero vincule un dispositivo bluetooth", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        btnDispConnect.setOnClickListener {
+            try {
+                if (m_bluetoothSocket == null || !m_isConnected) {
+
+                    val IntValSpin = listDispBT.selectedItemPosition
+                    m_address = mAddressDevices!!.getItem(IntValSpin).toString()
+                    Toast.makeText(this, m_address, Toast.LENGTH_LONG).show()
+                    // Cancel discovery because it otherwise slows down the connection.
+                    mBtAdapter?.cancelDiscovery()
+                    val device: BluetoothDevice = mBtAdapter.getRemoteDevice(m_address)
+                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
+                    m_bluetoothSocket!!.connect()
+                }
+
+                Toast.makeText(this, "CONEXION EXITOSA", Toast.LENGTH_LONG).show()
+                Log.i("MainActivity", "CONEXION EXITOSA")
+
+            } catch (e: IOException) {
+                //connectSuccess = false
+                e.printStackTrace()
+                Toast.makeText(this, "ERROR DE CONEXION", Toast.LENGTH_LONG).show()
+                Log.i("MainActivity", "ERROR DE CONEXION")
+            }
+        }
     }
 }
